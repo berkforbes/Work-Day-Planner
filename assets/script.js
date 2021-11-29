c1d4e2b0aecbf26f5495eb242dd9f3a9f3e0228d
Berk Forbes <berkforbes@yahoo.com>
2021-11-28 20:53:50 -0700

    local storage save via click of icon

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,38 @@
+//js moment to display current time
 $(document).ready(function () {
 var rightNow = moment().format("MMMM Do, YYYY - hh:mm a");
-$("#currentDay").html(rightNow); 
-//Show current date and time in header
-//currentDayEl = document.querySelector("#currentDay");
-//currentDayEl.textContent = rightNow;
 var currentHour = moment().format("HH");
 
-$("#clearCalendarBtn").click(function(event) {
+//Show current date and time in heade
+$("#currentDay").html(rightNow); 
+
+
+//button to clear text from calendar
+$("#clearPlannerBtn").click(function(event) {
   $("textarea").val("");
   localStorage.clear();
+});
 
-})
+//Thumbtack button saves to local storage
+$(".saveBtn").click(function(event) {
+  event.preventDefault
+  var taskItem = $(this).siblings(".task").val();
+  var timeOfDay = $(this).parent().attr("id");  
+  localStorage.setItem(taskItem, timeOfDay)
+});
+
+//Local storage retrieval
+$("#hour1 .task").val(localStorage.getItem("hour1"))
+$("#hour2 .task").val(localStorage.getItem("hour2"))
+$("#hour3 .task").val(localStorage.getItem("hour3"))
+$("#hour4 .task").val(localStorage.getItem("hour4"))
+$("#hour5 .task").val(localStorage.getItem("hour5"))
+$("#hour6 .task").val(localStorage.getItem("hour6"))
+$("#hour7 .task").val(localStorage.getItem("hour7"))
+$("#hour8 .task").val(localStorage.getItem("hour8"))
+$("#hour9 .task").val(localStorage.getItem("hour9"))
+$("#hour10 .task").val(localStorage.getItem("hour10"))
+
+
+
 })
