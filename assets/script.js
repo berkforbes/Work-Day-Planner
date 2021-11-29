e03a5d7b387f6769314706c42d72a8ca5f8deb4b
Berk Forbes <berkforbes@yahoo.com>
2021-11-28 17:48:17 -0700

    rework div containers for time blocks

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,9 @@
-var rightNow = moment().format("MMMM Do, YYYY - hh:mm:ss a");
+var rightNow = moment().format("MMMM Do, YYYY - hh:mm a");
 currentDayEl = document.querySelector("#currentDay");
+var currentHour = moment().format("HH");
 
 //Show current date and time in header
 currentDayEl.textContent = rightNow;
+
 
 
